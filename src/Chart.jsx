d711f302dc89836547ec73eea4b277b57d9ac695
xynyx <matt.taylr22@gmail.com>
2020-06-22 15:21:48 -0400

    Made chart responsive

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -7,36 +7,37 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  ResponsiveContainer,
 } from "recharts";
 
 export default function Chart({ mapData }) {
   const renderLineChart = (
-    <LineChart
-      width={700}
-      height={300}
-      data={mapData}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="date"/>
-      <YAxis tick={{fontSize: "1.3em"}}/>
-      <Tooltip />
-      <Legend />
-      <Line
-        animationDuration={500}
-        strokeWidth={2.3}
-        type="monotone"
-        dataKey="Exchange"
-        stroke="#8884d8"
-        dot={false}
-      />
-    </LineChart>
+    <ResponsiveContainer width="85%" height={300}>
+      <LineChart
+        data={mapData}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis tick={{ fontSize: "1.3em" }} />
+        <Tooltip />
+        <Legend />
+        <Line
+          animationDuration={500}
+          strokeWidth={2.3}
+          type="monotone"
+          dataKey="Exchange"
+          stroke="#8884d8"
+          dot={false}
+        />
+      </LineChart>
+    </ResponsiveContainer>
   );
 
-  return <div>{renderLineChart}</div>;
+  return renderLineChart;
 }
